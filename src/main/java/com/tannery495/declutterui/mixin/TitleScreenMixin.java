45595376f6d6c2435a4e5694fd08f01f24c0ec4c
tannery495 <g.tannery495@passmail.com>
2026-06-24 10:48:16 +0200

    Add Hide Language Button and fix button hiding

--- src/main/java/com/tannery495/declutterui/mixin/TitleScreenMixin.java
+++ src/main/java/com/tannery495/declutterui/mixin/TitleScreenMixin.java
@@ -3,7 +3,9 @@ package com.tannery495.declutterui.mixin;
 import com.tannery495.declutterui.Config;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphicsExtractor;
+import net.minecraft.client.gui.components.CommonButtons;
 import net.minecraft.client.gui.components.SplashRenderer;
+import net.minecraft.client.gui.components.SpriteIconButton;
 import net.minecraft.client.gui.screens.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,6 +13,26 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(TitleScreen.class)
 public abstract class TitleScreenMixin {
+
+    @Redirect(
+        method = "init",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/client/gui/components/CommonButtons;language(ILnet/minecraft/client/gui/components/Button$OnPress;Z)Lnet/minecraft/client/gui/components/SpriteIconButton;"
+        )
+    )
+    private SpriteIconButton hideLanguageButton(
+            int width,
+            net.minecraft.client.gui.components.Button.OnPress onPress,
+            boolean showLabel
+    ) {
+        SpriteIconButton button = CommonButtons.language(width, onPress, showLabel);
+        if (Config.HIDE_LANGUAGE_BUTTON.get()) {
+            button.visible = false;
+            button.active = false;
+        }
+        return button;
+    }
 
     @Redirect(
         method = "extractRenderState",
