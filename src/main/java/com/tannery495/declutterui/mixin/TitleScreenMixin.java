1088922a7276f3e4d891ce0df97fb77f039e3cba
tannery495 <g.tannery495@passmail.com>
2026-06-24 10:35:56 +0200

    Add Hide Title Screen Version Text

--- src/main/java/com/tannery495/declutterui/mixin/TitleScreenMixin.java
+++ src/main/java/com/tannery495/declutterui/mixin/TitleScreenMixin.java
@@ -9,7 +9,9 @@ import net.minecraft.client.gui.components.SpriteIconButton;
 import net.minecraft.client.gui.screens.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
 public abstract class TitleScreenMixin {
@@ -44,6 +46,13 @@ public abstract class TitleScreenMixin {
     private void skipSplashRender(SplashRenderer splash, GuiGraphics guiGraphics, int screenWidth, Font font, int color) {
         if (!Config.HIDE_SPLASH_TEXT.get()) {
             splash.render(guiGraphics, screenWidth, font, color);
+        }
+    }
+
+    @Inject(method = {"lambda$render$13", "lambda$render$14"}, at = @At("HEAD"), cancellable = true)
+    private void hideVersionText(GuiGraphics guiGraphics, int alpha, Integer line, String text, CallbackInfo ci) {
+        if (Config.HIDE_VERSION_TEXT.get()) {
+            ci.cancel();
         }
     }
 }
