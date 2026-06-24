a1f0765005108e3e1edbcb30623db461beb5d3e2
tannery495 <g.tannery495@passmail.com>
2026-06-24 10:48:51 +0200

    Add Hide Selected Item Name Popup

---
+++ src/main/java/com/tannery495/declutterui/mixin/GuiMixin.java
@@ -0,0 +1,21 @@
+package com.tannery495.declutterui.mixin;
+
+import com.tannery495.declutterui.Config;
+import net.minecraft.client.DeltaTracker;
+import net.minecraft.client.gui.Gui;
+import net.minecraft.client.gui.GuiGraphicsExtractor;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Gui.class)
+public abstract class GuiMixin {
+
+    @Inject(method = "maybeExtractSelectedItemName", at = @At("HEAD"), cancellable = true)
+    private void hideSelectedItemName(GuiGraphicsExtractor graphics, DeltaTracker deltaTracker, CallbackInfo ci) {
+        if (Config.HIDE_SELECTED_ITEM_NAME.get()) {
+            ci.cancel();
+        }
+    }
+}
